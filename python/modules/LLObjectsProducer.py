40545c7a5847cb65c500e71ed50259260b0076b6
Matthew <mkilpatr@FNAL.GOV>
2019-09-06 15:19:52 -0500

    Cleanup LLObjectProducer

--- python/modules/LLObjectsProducer.py
+++ python/modules/LLObjectsProducer.py
@@ -46,29 +46,16 @@ class LLObjectsProducer(Module):
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-	self.out.branch("Stop0l_nJets_HighPt",  		"I")
-	self.out.branch("Stop0l_nJets30",  			"I")
 	self.out.branch("Stop0l_nbtags_Loose",  		"I")
 	self.out.branch("Stop0l_MtLepMET", 			"F")
 	self.out.branch("nLeptonVeto",    			"I")
-	self.out.branch("Stop0l_nIsoTracksLep", 		"I")
-	self.out.branch("Stop0l_nIsoTracksHad", 		"I")
-	self.out.branch("Stop0l_nIsoTracksHad_ptgeq20", 	"I")
-	self.out.branch("Stop0l_nVetoElec", 			"I")
-	self.out.branch("Stop0l_nVetoMuon", 			"I")
 	self.out.branch("Stop0l_nVetoElecMuon", 		"I")
 	self.out.branch("Stop0l_noMuonJet",			"O")
 	self.out.branch("Pass_dPhiMETMedDM", 			"O")
 	self.out.branch("Pass_dPhiQCD",				"O")
 	self.out.branch("Pass_dPhiQCDSF",			"O")
-	self.out.branch("Pass_dPhiQCD_UCSB",			"O")
-	self.out.branch("Pass_dPhiQCDSF_UCSB",			"O")
 	self.out.branch("Stop0l_dPhiISRMET",			"F")
-	self.out.branch("Stop0l_TauPOG",			"I")
-	self.out.branch("Pass_HT30", 				"O")
-	self.out.branch("Pass_dPhiMET30", 			"O")
-	self.out.branch("Pass_dPhiMETLowDM30", 			"O")
-	self.out.branch("Pass_dPhiMETHighDM30", 		"O")
+	#self.out.branch("Stop0l_TauPOG",			"I")
 	self.out.branch("Jet_nsortedIdx", 			"I")
 	self.out.branch("Jet_sortedIdx", 			"I", lenVar="Jet_nsortedIdx")
 
@@ -87,16 +74,6 @@ class LLObjectsProducer(Module):
 
     def SelJets(self, jet):
         if jet.pt < 20 or math.fabs(jet.eta) > 2.4 :
-            return False
-        return True
-
-    def SelJets30(self, jet):
-        if jet.pt < 30 or math.fabs(jet.eta) > 2.4 :
-            return False
-        return True
-
-    def SelJetsHighPt(self, jet):
-        if jet.pt < 75 or math.fabs(jet.eta) > 2.4 :
             return False
         return True
 
@@ -143,10 +120,6 @@ class LLObjectsProducer(Module):
 		return False
 	return True
 
-    def CalHT(self, jets, jetpt):
-	HT = sum([j.pt for i, j in enumerate(jets) if (self.Jet_Stop0l[i] and j.pt > jetpt)])
-	return HT
-
     def SelNoMuon(self, jets, met):
 	noMuonJet = True
 	for j in jets:
@@ -171,15 +144,10 @@ class LLObjectsProducer(Module):
 	fatjets   = Collection(event, "FatJet")
 
         ## Selecting objects
-	self.Jet_Stop0lHighPt= map(self.SelJetsHighPt, jets)
 	self.Jet_Stop0l      = map(self.SelJets, jets)
-	self.Jet_Stop0l30    = map(self.SelJets30, jets)
 	local_BJet_Stop0l    = map(self.SelBtagJets, jets)
         self.BJet_Stop0l     = [a and b for a, b in zip(self.Jet_Stop0l, local_BJet_Stop0l )]
 	mt 		     = self.SelMtlepMET(electrons, muons, isotracks, met)
-	countIskLep 	     = sum([(i.Stop0l and (abs(i.pdgId) == 11 or abs(i.pdgId) == 13)) for i in isotracks])
-	countIskHad 	     = sum([(i.Stop0l and abs(i.pdgId) == 211) for i in isotracks])
-	countIskHad_ptgeq20  = sum([(i.Stop0l and abs(i.pdgId) == 211 and i.pt > 20) for i in isotracks])
 	countEle	     = sum([e.Stop0l for e in electrons])
 	countMuon	     = sum([m.Stop0l for m in muons])
 	noMuonJet	     = self.SelNoMuon(jets, met)
@@ -187,42 +155,21 @@ class LLObjectsProducer(Module):
 	PassdPhiMedDM        = self.PassdPhiVal(sortedPhi, [0.15, 0.15, 0.15], [0.5, 4., 4.]) #Variable for LowDM Validation bins
 	PassdPhiQCD          = self.PassdPhi(sortedPhi, [0.1, 0.1, 0.1], invertdPhi =True)
 	PassdPhiQCDSF        = self.PassdPhi(sortedPhi, [0.1, 0.1], invertdPhi =True)
-	sortedIdxQCD, sortedPhiQCD = self.GetJetSortedIdx(jets, 20, 2.4)
-	PassdPhiQCD_UCSB     = self.PassdPhi(sortedPhiQCD, [0.1, 0.1, 0.1], invertdPhi =True)
-	PassdPhiQCDSF_UCSB   = self.PassdPhi(sortedPhiQCD, [0.1, 0.1], invertdPhi =True)
 	dphiISRMet	     = abs(deltaPhi(fatjets[stop0l.ISRJetIdx].phi, met.phi)) if stop0l.ISRJetIdx >= 0 else -1
 	#self.Tau_Stop0l     = map(self.SelTauPOG, tau)
 	#countTauPOG	     = sum(self.Tau_Stop0l)
-	HT 		     = self.CalHT(jets, 30)
-	PassHT30	     = HT >= 300
-	sortIdx30, sortedPhi30= self.GetJetSortedIdx(jets, 30)
-	PassdPhiLowDM30      = self.PassdPhi(sortedPhi30, [0.5, 0.15, 0.15])
-	PassdPhiHighDM30     = self.PassdPhi(sortedPhi30, [0.5, 0.5, 0.5, 0.5])
 
         ### Store output
-	self.out.fillBranch("Stop0l_nJets_HighPt",    	sum(self.Jet_Stop0lHighPt))
-	self.out.fillBranch("Stop0l_nJets30",    	sum(self.Jet_Stop0l30))
 	self.out.fillBranch("Stop0l_nbtags_Loose",   	sum(self.BJet_Stop0l))
 	self.out.fillBranch("Stop0l_MtLepMET",  	mt)
 	self.out.fillBranch("nLeptonVeto",    		countMuon + countEle + countIskLep)
-	self.out.fillBranch("Stop0l_nIsoTracksLep",	countIskLep)
-	self.out.fillBranch("Stop0l_nIsoTracksHad",	countIskHad)
-	self.out.fillBranch("Stop0l_nIsoTracksHad_ptgeq20",	countIskHad_ptgeq20)
-	self.out.fillBranch("Stop0l_nVetoElec", 	countEle)
-	self.out.fillBranch("Stop0l_nVetoMuon", 	countMuon)
 	self.out.fillBranch("Stop0l_nVetoElecMuon", 	countEle + countMuon)
 	self.out.fillBranch("Stop0l_noMuonJet",		noMuonJet)
 	self.out.fillBranch("Pass_dPhiMETMedDM", 	PassdPhiMedDM)
 	self.out.fillBranch("Pass_dPhiQCD",		PassdPhiQCD)
 	self.out.fillBranch("Pass_dPhiQCDSF",		PassdPhiQCDSF)
-	self.out.fillBranch("Pass_dPhiQCD_UCSB",	PassdPhiQCD_UCSB)
-	self.out.fillBranch("Pass_dPhiQCDSF_UCSB",	PassdPhiQCDSF_UCSB)
 	self.out.fillBranch("Stop0l_dPhiISRMET",	dphiISRMet)
 	#self.out.fillBranch("Stop0l_TauPOG",		countTauPOG)
-	self.out.fillBranch("Pass_HT30",		PassHT30)
-	self.out.fillBranch("Pass_dPhiMET30", 		PassdPhiLowDM30)
-	self.out.fillBranch("Pass_dPhiMETLowDM30", 	PassdPhiLowDM30)
-	self.out.fillBranch("Pass_dPhiMETHighDM30", 	PassdPhiHighDM30)
 	self.out.fillBranch("Jet_nsortedIdx", 		len(sortedIdx))
 	self.out.fillBranch("Jet_sortedIdx", 		sortedIdx)
 	return True
