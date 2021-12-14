de0570818123e6505d1b358d7fd1b95f363f079a
Matthew <mkilpatr@FNAL.GOV>
2021-12-14 17:49:58 -0600

    Confirming all files are stored correctly

--- python/processors/Condor/SubmitLPC.py
+++ python/processors/Condor/SubmitLPC.py
@@ -183,7 +183,7 @@ def my_process(args):
     ## temp dir for submit
     global tempdir
     global ProjectName
-    ProjectName = time.strftime('%b%d') + ShortProjectName + VersionNumber + args.process + args.region + "_v16"
+    ProjectName = time.strftime('%b%d') + ShortProjectName + VersionNumber + args.process + args.region + "_again"
     if args.era == 0:
         tempdir = tempdir + os.getlogin() + "/" + ProjectName +  "/"
     else:
